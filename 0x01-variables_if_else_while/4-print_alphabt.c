17b53920bd6469a9358bca56014a1e4a9d7c1809
khalilholberton <1071@holbertonschool.com>
2019-09-19 20:10:44 +0000

    fix

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-  int i;
-  for (i = 'a'; i <= 'z'; i++)
-    if (i != 'e' && i != 'q')
-      putchar(i);
-  putchar('\n');
-  return (0);
+int i;
+for (i = 'a'; i <= 'z'; i++)
+if (i != 'e' && i != 'q')
+putchar(i);
+putchar('\n');
+return (0);
 }
