ecefcd1b19a04475779afb4b52b08d1d2ef5965e
1071holbertonschool.com <1071@holbertonschool.com>
2019-09-24 11:07:58 +0100

    change

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,17 +9,17 @@
 void jack_bauer(void)
 {
 
-int i,j;
+int i, j;
 
 for (i = 0; i < 24; i++)
-{ 
-for (j = i+1; j < 60; j++)
-{	
+{
+for (j = i + 1; j < 60; j++)
+{
 _putchar(i / 10 + '0');
 _putchar(i % 10 + '0');
 _putchar(':');
 _putchar(j / 10 + '0');
-_putchar(j % 10 + '0'); 
+_putchar(j % 10 + '0');
 _putchar('\n');
 }
 }
