ec427794811cab1d0d68ba64ebb3c037daaaea8f
1071holbertonschool.com <1071@holbertonschool.com>
2019-09-24 11:10:26 +0100

    change

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 
 #include "holberton.h"
 /**
-   * main - check the code for Holberton School students.
+   * jack_bauer - check the code for Holberton School students.
     *
      * Return: Always 0.
       */
