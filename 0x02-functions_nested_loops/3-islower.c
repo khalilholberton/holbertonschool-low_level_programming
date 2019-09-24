24f129a35b293b2406b8befe52b41e4d3f31ee9f
1071holbertonschool.com <1071@holbertonschool.com>
2019-09-24 10:11:58 +0100

    change

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
-
 /**
- *_islower - checks if character is lowercase, return 1 is yes and 0 if not.
+ * _islower - checks if character is lowercase, return 1 is yes and 0 if not.
+ * @c: is char type
  * Return: 1 if c is lower and 0 otherwise.
  */
 
