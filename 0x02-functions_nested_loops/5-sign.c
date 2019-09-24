c4f33dada979273e43f0e576a073bbf4db2d0543
1071holbertonschool.com <1071@holbertonschool.com>
2019-09-24 10:28:56 +0100

    change

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
+ * print_sign -check if n is +, - or 0.
+ * @n: int type.
  * Returns -1 and prints - if n is less than zero.
  * Returns 0 and prints 0 if n is zero.
  * Returns 1 and prints + if n is greater than zero.
- * @n: int type.
- * print_sign -check if n is +, - or 0
  */
 
 int print_sign(int n)
