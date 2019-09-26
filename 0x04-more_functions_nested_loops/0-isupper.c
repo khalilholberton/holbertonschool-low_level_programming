ad5ac17a60207febb44343993927bba55f548d02
1071holbertonschool.com <1071@holbertonschool.com>
2019-09-26 12:46:45 +0100

    fix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
-include "holberton.h"
+#include "holberton.h"
 /**
  * isupper- check for uppercase character.
  * @c :is char type.
  * Return: 1 if c uppercase
  * and 0 if otherwise.
  */
-int isupper(int c)
+int _isupper(int c)
 {
 if (c >= 65 && c <= 90)
 return (1);
