402328989b78c7a003207e1b38b94921ccd14e95
1071holbertonschool.com <1071@holbertonschool.com>
2019-10-01 11:19:12 +0100

    fix

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_rev -prints a string, in reverse, followed by a new line.
+ * @s : is char type.
+ */
+void print_rev(char *s)
+{
+int i;
+for (i = 0 ; s[i] != '\0'; i++)
+{}
+i = i - 1;
+while (s[i] != '\0')
+{
+_putchar(s[i]);
+i--;
+}
+_putchar('\n');
+}
