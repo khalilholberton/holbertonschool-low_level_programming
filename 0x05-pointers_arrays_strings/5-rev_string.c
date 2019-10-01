4447cbdf01b43ca5b0953b7a31f9ee811c3fd6e5
1071holbertonschool.com <1071@holbertonschool.com>
2019-10-01 12:44:33 +0100

    fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,12 +12,10 @@ char swapcontainer;
 for (i = 0 ; s[i] != '\0'; i++)
 {}
 i = i - 1;
-while (j < i)
+ for(j = 0 ; j < i/2; j++)
 {
 swapcontainer = s[i];
 s[i] = s[j];
 s[j] = swapcontainer;
-i--;
-j++;
 }
 }
