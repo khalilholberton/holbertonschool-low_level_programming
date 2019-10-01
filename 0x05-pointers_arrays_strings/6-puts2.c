e10c39aef49e653f06985d18ae3f7ac68ba39804
1071holbertonschool.com <1071@holbertonschool.com>
2019-10-01 14:17:48 +0100

    fix

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ * @str : is char type.
+ */
+void puts2(char *str)
+{
+int j;
+for (j = 0; str[j] != 0 ; j++)
+{
+if (j  % 2 == 0)
+{
+_putchar(str[j]);
+}
+}
+_putchar('\n');
+}
