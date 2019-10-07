1d5228e4089ea2098a74ee0b2f4c15187c37421a
1071holbertonschool.com <1071@holbertonschool.com>
2019-10-07 15:52:29 +0100

    fix

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * *_strchr - check the code for Holberton School students.
+ * @s : is char type
+ * @c : is char type
+ * Return: null if caracter not found.
+ */
+char *_strchr(char *s, char c)
+{
+int i = 0;  
+while (s[i] != '\0')
+{
+if  (s[i] == c)
+{
+return (&(s[i]));
+}
+i++; 
+}
+return (0);
+}
