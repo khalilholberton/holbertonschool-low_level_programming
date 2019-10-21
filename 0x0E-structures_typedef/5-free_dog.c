4bf611614a210662f671f52dc924582c4c86e994
khalilholberton <1071@holbertonschool.com>
2019-10-21 22:50:01 +0000

    fix

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - function frees dogs
+ * @d: struct dog_t
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+
+if (d)
+{
+free(d->owner);
+free(d->name);
+free(d);
+}
+}
