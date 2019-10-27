69877071369cb8b255ff5f88c2c5116ebd7469c8
khalilholberton <1071@holbertonschool.com>
2019-10-27 17:25:37 +0000

    fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,8 +1,8 @@
 #include "variadic_functions.h"
 /**
-* sum_them_all - sum of all its parameters
-* @n: unsigned int type
-* Return: sum.
+*sum_them_all - sum of all its parameters
+*@n: unsigned int type
+*Return: sum.
 */
 int sum_them_all(const unsigned int n, ...)
 {
