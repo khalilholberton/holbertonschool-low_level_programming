d2854f1e5f6a962544f398b7044a688bdd52b851
khalilholberton <1071@holbertonschool.com>
2019-10-28 15:19:38 +0000

    fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,16 +1,14 @@
 #include <stdarg.h>
 /**
- *sum_them_all - sums all given arguments
- *@n: takes integer also a variable of argument list
- *@...: ellipsis
- *Return: sum of arguments
- */
+*sum_them_all - sums all given arguments
+*@n: takes integer also a variable of argument list
+*@...: ellipsis
+*Return: sum of arguments
+*/
 int sum_them_all(const unsigned int n, ...)
 {
 unsigned int i, sum = 0;
-
 va_list arg;
-
 if (n == 0)
 return (0);
 va_start(arg, n);
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,7 +10,7 @@
 typedef struct  oper
 {
   char *c;
-  void (*fun)(va_list);
+  void (*fun)();
 } oper_t;
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
