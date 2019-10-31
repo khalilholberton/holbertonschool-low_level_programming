3b9ca13c299a8ad74b71318263c2b0c257efd9a0
khalilholberton <1071@holbertonschool.com>
2019-10-31 18:29:54 +0000

    fix it

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void print_first(void) __attribute__ ((constructor));
+
+/**
+ * print_first - prints message before main
+ *
+ * Return: void
+ */
+void print_first(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
