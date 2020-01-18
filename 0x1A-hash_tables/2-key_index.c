e568542ada44d3410e305cd245d2940003ee78bb
khalilholberton <1071@holbertonschool.com>
2020-01-18 23:25:33 +0000

    fix

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,6 +4,7 @@
  * key_index - function that gives you the index of a key
  * @key : is the key
  * @size: is the size of the array of the hash table
+ * Return: index of the key
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
