c27d5b99a7a997774c3ffa0651bca5eef0ad6739
khalilholberton <1071@holbertonschool.com>
2020-01-18 23:10:20 +0000

    fix

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 -  function that implements the djb2 algorithm
+ * @str : const unsigned char type
+ * Return: always succesful
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	{
+		unsigned long hash = 5381;
+		int c;
+
+		while ((c = *str++))
+		{
+			hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+		}
+		return (hash);
+	}
+}
