014f065696e164c4551eb79077d11f4151f360f7
khalilholberton <1071@holbertonschool.com>
2020-01-18 22:55:17 +0000

    fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	tab->size = size;
-	tab->array = malloc(sizeof(hash_node_t) * size);
+	tab->array = malloc(sizeof(hash_node_t *) * size);
 	if (tab->array == NULL)
 	{
 		free(tab);
