2849b9e754cf1ebd48f8bc9d456c2c7b92d7dd70
khalilholberton <1071@holbertonschool.com>
2020-01-19 18:55:23 +0000

    fix

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,27 +11,29 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned int i;
 
 	i = 0;
-
-	unsigned int y;
-
-	y = 0;
+	int y;
 
 	hash_node_t *tempo;
 
-	if (ht == NULL)
+	if (!ht)
 		return;
+	y = 0;
 	printf("{");
-	for (i = 0; i < ht->size; i++)
+	while (i < ht->size)
 	{
 		tempo = ht->array[i];
 		while (tempo)
 		{
 			if (y == 1)
+				y = 1;
+			else
+			{
 				printf(", ");
+			}
 			printf("'%s': '%s'", tempo->key, tempo->value);
-			y = 1;
 			tempo = tempo->next;
 		}
+		i++;
 
 	}
 	printf("}\n");
