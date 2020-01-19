28864e2381c4bfd837db3959475620e088d6c5c9
khalilholberton <1071@holbertonschool.com>
2020-01-19 16:58:35 +0000

    fix

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -2,9 +2,9 @@
 
 /**
  * hash_table_get - function that retrieves a value associated with a key.
+ * @ht : hash table
  * @key : the key to looking for
- *
- * Return : value or NULL if key couldn’t be found
+ * Return: value or NULL if key couldn’t be found
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
