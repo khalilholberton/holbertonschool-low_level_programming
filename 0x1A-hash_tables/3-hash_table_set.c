6a40bef36805eac5c2516f94918fda419cba4a8a
khalilholberton <1071@holbertonschool.com>
2020-01-19 17:37:28 +0000

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,7 +27,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		;
 	if (count == 0)
 		return (0);
-	node = malloc(sizeof(hash_node_t));
+	node = malloc(sizeof(hash_node_t *));
 	if (!node)
 		return (0);
 
